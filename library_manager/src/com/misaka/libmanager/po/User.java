9072758e6dd75370cef639c1e753927abc548991
lixuwei2333 <30304550+lixuwei2333@users.noreply.github.com>
2020-04-28 21:21:18 +0800

    Update User.java

--- library_manager/src/com/misaka/libmanager/po/User.java
+++ library_manager/src/com/misaka/libmanager/po/User.java
@@ -10,33 +10,4 @@ public class User {
 		this.password = password;
 		this.admin = admin;
 	}
-	public User(String account, String password) {
-		super();
-		this.account = account;
-		this.password = password;
-	}
-
-
-	public String getAccount() {
-		return account;
-	}
-	public void setAccount(String account) {
-		this.account = account;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public int getAdmin() {
-		return admin;
-	}
-	public void setAdmin(int admin) {
-		this.admin = admin;
-	}
-	public String toString() {
-		String re ="'"+account+"'"+","+"'"+password+"'"+",0";
-		return re;
-	}
 }
